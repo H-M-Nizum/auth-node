cc8cea171d36adead3779185b99c17364d99c793
H-M-Nizum <hmnizum1714032@gmail.com>
2025-03-09 21:34:09 +0600

    Configer index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("000000000000000");
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+// App
+const app = express();
+
+//
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser);
+app.use(express.urlencoded({ extended: true }));
+
+// DB Connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Root Route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Node" });
+});
+
+// Listener
+app.listen(process.env.PORT, () => {
+  console.log("Listening....");
+});
